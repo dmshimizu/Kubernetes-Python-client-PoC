f514c8f2efdd2a62e5b00ee08fc7445122364351
Diego Massola Shimizu <dmshimizu@Diegos-MacBook-Pro.local>
2021-11-14 13:20:53 -0500

    Create a didatic project from Kubernetes Python client PoC

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 RUN adduser --disabled-password --gecos "" worker
 COPY --chown=worker:worker kube_config .
-COPY --chown=worker:worker pi-calculator.py .
+COPY --chown=worker:worker k8s-job-creator.py .
--- k8s-job-creator.py
+++ k8s-job-creator.py
@@ -1,17 +1,27 @@
 from kubernetes import client, config
 from pprint import pprint
 
-def main():
+if __name__ == '__main__':
+    # Load k8s config file, containing target cluster configuration
     config.load_kube_config(config_file="kube_config")
-    container = client.V1Container(name="pi", image="perl",
+
+    # Define the pi-container container spec. This container will calculate pi
+    container = client.V1Container(name="pi-container", image="perl",
         command=["perl", "-Mbignum=bpi", "-wle", "print bpi(2000)"])
+
+    # Define pod spec to run pi container
     template = client.V1PodTemplateSpec(spec=client.V1PodSpec(containers=[container], restart_policy="Never"))
+
+    # Define pi-job job spec. This job will create a pod to calculate pi
     job = client.V1Job(api_version="batch/v1", kind="Job",
-        metadata=client.V1ObjectMeta(name="pi"),
+        metadata=client.V1ObjectMeta(name="pi-job"),
         spec=client.V1JobSpec(template=template, backoff_limit=4))
+    
     batch_v1 = client.BatchV1Api()
+
+    # K8s API call that creates the pi-job, as specified in this program
     api_response = batch_v1.create_namespaced_job(body=job, namespace="default")
+
+    # Print K8s API response, for debugging purposes
     pprint(api_response)
 
-if __name__ == '__main__':
-    main()
